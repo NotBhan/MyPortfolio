fc505d333d0a456505e3bd3def1b6d212822b7bc
CHANDRABHAN <chandrabhanmahato56@gmail.com>
2025-06-25 10:45:49 +0530

    Removed old project

--- myproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    # path('hello/', views.hello_view, name='hello'),
-    path('student_form/', views.student_form, name='student_form'),
-]
-
